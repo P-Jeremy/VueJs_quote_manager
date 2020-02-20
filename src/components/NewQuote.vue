2547bda9561ed0c681e9fa9c0fb4945b8d6ee5d5
Jeremy_P <jemyplu@gmail.com>
2020-02-20 11:36:47 +0100

    input styling improvments

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="row">
     <form action>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-offset-3">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-offset-3 form-group">
         <label for="quoteInput">Quote</label>
         <textarea class="form-control" name id="quoteInput" rows="3" v-model="quote"></textarea>
       </div>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-offset-3">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-offset-3 form-group">
         <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
       </div>
     </form>
