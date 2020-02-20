129626d2d071aebcc2dd3a9b0e419d004da8b7af
Jeremy_P <jemyplu@gmail.com>
2020-02-20 10:26:23 +0100

    progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
+  <app-header></app-header>
     <app-new-quote @newQuote="addQuote"></app-new-quote>
     <app-grid @deleteQuote="removeQuote" :quotes="quotes"></app-grid>
-    <div class="row">
+    <div v-if="this.quotes.length" class="row">
       <div class="col-sm-12 text-center">
-        <div class="alert alert-info">
-          Info: Click on a quote to delete it
-        </div>
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
       </div>
     </div>
   </div>
@@ -15,20 +14,21 @@
 <script>
 import QuotesGrid from './components/QuoteGrid'
 import NewQuote from './components/NewQuote'
+import Header from './components/Header'
 
 export default {
   components: {
     appGrid: QuotesGrid,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appHeader: Header
   },
   data () {
     return {
       maxQuotes: 10,
-      quotes: [
-        'Paris is always a good idea'
-      ]
+      quotes: ['Paris is always a good idea']
     }
   },
+
   methods: {
     addQuote (txt) {
       this.quotes.push(txt)
--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <form action>
-      <div class="col-sm_8 col-sm-offset-2 col-xs-12 col-md-offset-3">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-offset-3">
         <label for="quoteInput">Quote</label>
         <textarea class="form-control" name id="quoteInput" rows="3" v-model="quote"></textarea>
       </div>
